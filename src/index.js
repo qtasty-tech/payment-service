f6d97d12ff1829acbac95c882d50c10bb4a623a1
Thisal Deelaka <165908864+ThisalDeelaka@users.noreply.github.com>
2025-05-02 20:26:02 +0530

    added .env

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8082;
 
 app.use(express.json()); // To parse JSON bodies
 app.use(cors())
